150f59927b76e6055db03890e886848362e00748
TulioBuarque <67755514+TulioBuarque@users.noreply.github.com>
2025-05-06 14:57:28 +0200

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Página não encontrada (404)</h1>
+    </div>
+  )
+}
