27835f7b79dcba40e56b78402eb97b0a33592faf
TulioBuarque <67755514+TulioBuarque@users.noreply.github.com>
2025-05-06 15:05:16 +0200

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Página não encontrada (404)</h1>
+      <h1 className="text-2xl font-bold">Página não encontrada</h1>
     </div>
-  )
+  );
 }
