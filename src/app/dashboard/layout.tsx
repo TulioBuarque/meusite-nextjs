b09c0dffafe3688145eeecd079bfe3e9768af082
TulioBuarque <67755514+TulioBuarque@users.noreply.github.com>
2025-05-14 18:07:35 +0200

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import type React from "react"
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="p-6 space-y-8 max-w-7xl mx-auto">
